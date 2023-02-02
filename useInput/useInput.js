e29fe10a09537f4746400018cc7f572fab48add7
UHyun-K <konglim2@naver.com>
2023-02-02 16:12:32 +0900

    정리

---
+++ useInput/useInput.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+const useInput = (initialValue, validator) => {
+    const [value, setValue] = useState(initialValue);
+    const onChange = (event) => {
+        const {
+            target: { value },
+        } = event;
+        let willUpdate = true;
+        if (typeof validator === "function") {
+            willUpdate = validator(value);
+        }
+        if (willUpdate) {
+            setValue(value);
+        }
+    };
+    return { value, onChange }; //{value:value, onchange: onChange}
+};
+export default function App() {
+    const maxLeng = (value) => value.length <= 10;
+    const name = useInput("Mr.", maxLeng);
+    return (
+        <div className="App">
+            <h1>Hello CodeSandbox</h1>
+            <input placeholder="Name" {...name} />
+        </div>
+    );
+}
---
+++ useTabs/useTabs.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+const content = [
+    {
+        tab: "Section 1",
+        content: "I'm the content of the Section 1",
+    },
+    {
+        tab: "Section 2",
+        content: "I'm the content of the Section 2",
+    },
+];
+
+const useTabs = (initialTabs, allTabs) => {
+    const [currentIndex, setCurrentIndex] = useState(initialTabs);
+
+    if (!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+
+    return { currentItem: allTabs[currentIndex], changeItem: setCurrentIndex };
+};
+export default function useTabsApp() {
+    const { currentItem, changeItem } = useTabs(0, content);
+    return (
+        <div>
+            {content.map((section, index) => (
+                <button key={index} onClick={() => changeItem(index)}>
+                    {section.tab}
+                </button>
+            ))}
+            <div>{currentItem.content}</div>
+        </div>
+    );
+}
